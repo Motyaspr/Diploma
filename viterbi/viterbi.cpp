dfa54ced9f3c536faacc21b9bd4809b9723f0c7d
Motyaspr <sprikutm@gmail.com>
2020-12-02 15:05:44 +0300

    Add noise and decode for noised message

--- viterbi/viterbi.cpp
+++ viterbi/viterbi.cpp
@@ -9,20 +9,24 @@
 #include <set>
 #include <algorithm>
 #include "reed_muller.h"
+#include <iomanip>
+#include <map>
+#include <random>
+#include <cmath>
 
-using namespace std;
 
 const int INF = 10000;
 
-size_t get_first_one(vector<bool> t) {
+size_t get_first_one(std::vector<bool> t) {
     for (size_t i = 0; i < t.size(); i++)
         if (t[i])
             return i;
+    return INF;
 }
 
 struct matrix {
     size_t n{}, m{};
-    vector<vector<bool>> mt;
+    std::vector<std::vector<bool>> mt;
 
     matrix() {
         n = 0, m = 0;
@@ -46,7 +50,7 @@ struct matrix {
         }
     }
 
-    static bool span_cmp(vector<bool> a, vector<bool> b) {
+    static bool span_cmp(std::vector<bool> a, std::vector<bool> b) {
         return get_first_one(a) < get_first_one(b);
     }
 
@@ -71,7 +75,7 @@ struct matrix {
     }
 
     void change_last() {
-        vector<vector<size_t>> ends(m);
+        std::vector<std::vector<size_t>> ends(m);
         for (size_t i = 0; i < n; i++)
             for (size_t j = m - 1; j >= 0; j--)
                 if (mt[i][j]) {
@@ -96,13 +100,13 @@ struct matrix {
     void print() {
         for (size_t i = 0; i < n; i++) {
             for (size_t j = 0; j < m; j++)
-                cout << mt[i][j];
-            cout << "\n";
+                std::cout << mt[i][j];
+            std::cout << "\n";
         }
     }
 
-    vector<pair<size_t, size_t>> get_start_end() {
-        vector<pair<size_t, size_t>> ans;
+    std::vector<std::pair<size_t, size_t>> get_start_end() {
+        std::vector<std::pair<size_t, size_t>> ans;
         for (size_t i = 0; i < n; i++) {
             size_t first_ind = -1;
             size_t second_ind = -1;
@@ -119,9 +123,9 @@ struct matrix {
     }
 };
 
-vector<vector<size_t>> profile(matrix a) {
+std::vector<std::vector<size_t>> profile(matrix a) {
     auto indexes = a.get_start_end();
-    vector<vector<size_t>> lengths;
+    std::vector<std::vector<size_t>> lengths;
     lengths.resize(a.m + 1);
     for (size_t i = 0; i < indexes.size(); i++)
         for (size_t j = indexes[i].first; j < indexes[i].second; j++)
@@ -135,15 +139,15 @@ void add_bit(size_t &x, size_t i, size_t n) {
 }
 
 struct edge {
-    int w;
     size_t nxt;
+    int w;
 
     edge() {
         w = -1;
         nxt = 0;
     };
 
-    edge(size_t nxt1, int w1) : nxt(nxt1), w(w1) {};
+    edge(size_t  nxt1, int w1) : nxt(nxt1), w(w1) {};
 };
 
 struct vertex {
@@ -153,15 +157,15 @@ struct vertex {
     vertex() = default;
 };
 
-int calc_w(vector<pair<size_t, bool>> a, vector<pair<size_t, bool>> b) {
+int calc_w(std::vector<std::pair<size_t, bool>> a, std::vector<std::pair<size_t, bool>> b) {
     int res = 0;
     for (size_t i = 0; i < a.size(); i++)
         res = (res ^ (a[i].second & b[i].second));
     return res;
 }
 
-vector<vector<vertex>> create_graph(matrix t) {
-    vector<vector<vertex>> trellis;
+std::vector<std::vector<vertex>> create_graph(matrix t) {
+    std::vector<std::vector<vertex>> trellis;
     auto indexes = profile(t);
     trellis.resize(indexes.size());
     for (size_t i = 0; i < trellis.size(); i++) {
@@ -171,12 +175,12 @@ vector<vector<vertex>> create_graph(matrix t) {
     }
 
     for (size_t i = 0; i < trellis.size() - 1; i++) {
-        set<size_t> cur_nxt, cur;
+        std::set<size_t> cur_nxt, cur;
         for (size_t j = 0; j < indexes[i + 1].size(); j++)
             cur_nxt.insert(j);
         for (size_t j = 0; j < indexes[i].size(); j++)
             cur.insert(j);
-        vector<pair<size_t, size_t>> same;
+        std::vector<std::pair<size_t, size_t>> same;
         for (size_t fir = 0; fir < indexes[i].size(); ++fir)
             for (size_t sec = 0; sec < indexes[i + 1].size(); sec++)
                 if (indexes[i][fir] == indexes[i + 1][sec]) {
@@ -187,7 +191,7 @@ vector<vector<vertex>> create_graph(matrix t) {
                 }
         int diff_nxt = (cur_nxt.size() == 0 ? -1 : *cur_nxt.begin());
         int diff = (cur.size() == 0 ? -1 : *cur.begin());
-        vector<size_t> need;
+        std::vector<size_t> need;
         for (size_t j = 0; j < same.size(); j++) {
             need.push_back(indexes[i][same[j].first]);
         }
@@ -195,13 +199,13 @@ vector<vector<vertex>> create_graph(matrix t) {
             need.push_back(indexes[i + 1][diff_nxt]);
         if (diff != -1)
             need.push_back(indexes[i][diff]);
-        vector<pair<size_t, bool>> column;
+        std::vector<std::pair<size_t, bool>> column;
         for (size_t j = 0; j < need.size(); j++)
             column.push_back({need[j], t.mt[need[j]][i]});
         sort(column.begin(), column.end());
         for (size_t j = 0; j < trellis[i].size(); j++) {
             size_t to_gen = 0;
-            vector<pair<size_t, bool>> my_column;
+            std::vector<std::pair<size_t, bool>> my_column;
             for (size_t v = 0; v < same.size(); v++)
                 if (get_bit(j, same[v].first, indexes[i].size())) {
                     add_bit(to_gen, same[v].second, indexes[i + 1].size());
@@ -225,18 +229,25 @@ vector<vector<vertex>> create_graph(matrix t) {
     return trellis;
 }
 
-vector<size_t> decode(matrix t, vector<bool> code) {
+double get_prob(double x, bool y, double sigma_2) {
+    double t = 1 / sqrt(2 * M_PI * sigma_2);
+    double v = (y == 1) ? exp(-((x - 1) * (x - 1)) / (2 * sigma_2)) : exp(-((x + 1) * (x + 1)) / (2 * sigma_2));
+    return t * v;
+}
+
+std::vector<size_t> decode_bool(matrix &t, std::vector<bool> &code) {
     auto trellis = create_graph(t);
-    vector<vector<int>> dp, p;
+    std::vector<std::vector<int>> dp, p;
     dp.resize(trellis.size());
     p.resize(trellis.size());
     for (size_t i = 0; i < trellis.size(); i++) {
-        dp[i].resize(trellis[i].size(), INF);
+        dp[i].resize(trellis[i].size(), 0.0);
         p[i].resize(trellis[i].size(), -1);
     }
-    dp[0][0] = 0;
+    dp[0][0] = 1.0;
     for (size_t i = 0; i < dp.size() - 1; i++)
         for (size_t j = 0; j < dp[i].size(); j++) {
+
             if (dp[i + 1][trellis[i][j].zer.nxt] > dp[i][j] + (trellis[i][j].zer.w ^ code[i])) {
                 dp[i + 1][trellis[i][j].zer.nxt] = dp[i][j] + (trellis[i][j].zer.w ^ code[i]);
                 p[i + 1][trellis[i][j].zer.nxt] = j;
@@ -248,7 +259,7 @@ vector<size_t> decode(matrix t, vector<bool> code) {
                 }
             }
         }
-    vector<size_t> errs;
+    std::vector<size_t> errs;
     size_t curj = 0;
     for (size_t i = dp.size() - 1; i >= 1; i--) {
         if (dp[i][curj] != dp[i - 1][p[i][curj]])
@@ -259,18 +270,123 @@ vector<size_t> decode(matrix t, vector<bool> code) {
     return errs;
 }
 
+std::vector<bool>
+decode(std::vector<double> &code, double sigma_2, std::vector<std::vector<vertex>> trellis) {
+    std::vector<std::vector<int>> p;
+    std::vector<std::vector<double>> dp;
+    dp.resize(trellis.size());
+    p.resize(trellis.size());
+    for (size_t i = 0; i < trellis.size(); i++) {
+        dp[i].resize(trellis[i].size(), INF);
+        p[i].resize(trellis[i].size(), -1);
+    }
+    dp[0][0] = 0.0;
+    for (size_t i = 0; i < dp.size() - 1; i++) {
+        std::pair<double, double> ws = {get_prob(code[i], false, sigma_2), get_prob(code[i], true, sigma_2)};
+        for (size_t j = 0; j < dp[i].size(); j++) {
+            double w = (trellis[i][j].zer.w == 0) ? ws.first : ws.second;
+            //std::cout << code[i] << ' ' << trellis[i][j].zer.w << ' ' << w << "\n";
+            //std::cout << 0 << ' ' << trellis[i][j].zer.w << "\n";
+            if (dp[i + 1][trellis[i][j].zer.nxt] > dp[i][j] + w) {
+                dp[i + 1][trellis[i][j].zer.nxt] = dp[i][j] + w;
+                p[i + 1][trellis[i][j].zer.nxt] = j;
+            }
+            if (trellis[i][j].one.w != -1) {
+                w = (trellis[i][j].one.w == 0) ? ws.first : ws.second;;
+                if (dp[i + 1][trellis[i][j].one.nxt] > dp[i][j] + w) {
+                    dp[i + 1][trellis[i][j].one.nxt] = dp[i][j] + w;
+                    p[i + 1][trellis[i][j].one.nxt] = j;
+                }
+            }
+        }
+    }
+    std::vector<bool> ans;
+    size_t curj = 0;
+    for (size_t i = dp.size() - 1; i >= 1; i--) {
+        size_t new_curj = p[i][curj];
+        if (trellis[i - 1][new_curj].zer.nxt == curj)
+            ans.push_back(1 - trellis[i - 1][new_curj].zer.w);
+        else
+            ans.push_back(1 - trellis[i - 1][new_curj].one.w);
+        curj = new_curj;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+
+std::vector<bool> gen_rand_vect(size_t n) {
+    std::vector<bool> ans;
+    for (size_t i = 0; i < n; i++)
+        if (rand() % 2)
+            ans.push_back(true);
+        else
+            ans.push_back(false);
+    return ans;
+}
+
+std::vector<bool> code_matrix(matrix &a, const std::vector<bool> &code) {
+    std::vector<bool> res;
+    for (size_t i = 0; i < a.m; i++) {
+        bool ans = 0;
+        for (size_t j = 0; j < code.size(); j++)
+            ans ^= (a.mt[j][i] & code[j]);
+        res.push_back(ans);
+    }
+    return res;
+}
+
+std::vector<double> add_noise(std::vector<bool> &message, std::vector<double> &noise) {
+    std::vector<double> res;
+    for (size_t i = 0; i < message.size(); i++) {
+        if (!message[i])
+            res.push_back(-1 + noise[i]);
+        else
+            res.push_back(1 + noise[i]);
+    }
+    return res;
+}
+
+bool cmp(std::vector<bool> &a, std::vector<bool> &b) {
+    for (size_t i = 0; i < a.size(); i++)
+        if (a[i] != b[i])
+            return false;
+    return true;
+}
+
 
 int main() {
+    srand(time(NULL));
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+
     ReedMuller reedMuller(2, 5);
     matrix t(reedMuller.generated);
     t.print();
     t.to_span();
-    std::cout << "\n";
     t.print();
-//    auto ans = decode(t, code);
-//    cout << ans.size() << "\n";
-//    for (size_t i = 0; i < ans.size(); i++)
-//        cout << ans[i] << ' ';
+
+    std::vector<std::vector<vertex>> trellis = create_graph(t);
+
+    for (double Eb_N0_dB = 0; Eb_N0_dB <= 6.0; Eb_N0_dB += 1.0) {
+        int cnt = 0;
+        for (size_t i = 0; i < 10000000; i++) {
+            double sigma_square = 1.0 / (pow(10.0, Eb_N0_dB / 10));
+            std::normal_distribution<> d{0, sigma_square};
+            std::vector<bool> coded = code_matrix(t, gen_rand_vect(t.n));
+            std::vector<double> noise;
+            noise.reserve(coded.size());
+            for (size_t j = 0; j < coded.size(); j++)
+                noise.push_back(d(gen));
+            auto x = add_noise(coded, noise);
+            auto decoded = decode(x, sigma_square, trellis);
+            if (!cmp(coded, decoded))
+                cnt++;
+        }
+        std::cout << "\n";
+        std::cout << Eb_N0_dB << ' ' << (double) cnt / 1000000 << "\n";
+    }
+
 }
 
 
